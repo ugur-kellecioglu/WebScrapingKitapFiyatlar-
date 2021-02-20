e8001da3024cfb3b2887d48daeab07829692539b
ugur-kellecioglu <ugur.kellecioglu@outlook.com>
2021-02-20 13:06:57 +0300

    kitap ismi, resmi, fiyatı aranan kitaba göre gösteriliyor.

--- public/app.js
+++ public/app.js
@@ -8,14 +8,60 @@ async function getData(){
     }
     
     const results = document.querySelector('.results');
-    
+    let row = document.createElement('div');
+    row.className = "row";
     let btn = document.getElementById('submit');
     btn.addEventListener('click', () =>{
+        while(results.firstChild) results.removeChild(results.lastChild);
     getData().then(response => {
     response.forEach(element => {
-    let item = document.createElement('div');
-    item.className = 'item';
-    results.appendChild(item);
+
+       
+
+        let brand = element.brand;
+        let name = element.name;
+        let price = element.price;
+        let image = element.picture;
+
+        let brandEl = document.createElement('div');
+        brandEl.className = 'brand';
+        let brandtxt = document.createElement('h5');
+        brandtxt.innerHTML = brand;
+        brandEl.appendChild(brandtxt); 
+
+        let nameEl = document.createElement('div');
+        nameEl.className = 'name';
+        let nametxt = document.createElement('h5');
+        nametxt.innerHTML = name;
+        nameEl.appendChild(nametxt); 
+        
+        let priceEl = document.createElement('div');
+        priceEl.className = 'price';
+        let pricetxt = document.createElement('h5');
+        pricetxt.innerHTML = price;
+        priceEl.appendChild(pricetxt); 
+
+        let imageEl = document.createElement('div');
+        imageEl.className = 'picture';
+        let priceimg = document.createElement('img');
+        priceimg.setAttribute('src', image);
+        imageEl.appendChild(priceimg);        
+
+
+        let item = document.createElement('div');
+        item.className = 'item';
+
+        item.appendChild(imageEl);
+        item.appendChild(nameEl);
+        item.appendChild(brandEl);
+        item.appendChild(priceEl);
+
+        row.appendChild(item);
+        results.appendChild(row);
+
+       
+
+        console.log(element);
     });
     
     
