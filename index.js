6abd5e26020978cd16c3f6b378c453c264d2080e
ugur-kellecioglu <ugur.kellecioglu@outlook.com>
2021-02-21 10:29:36 +0300

    final

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const express  = require('express');
+const express = require('express');
 
 const app = express();
 
@@ -7,9 +7,9 @@ const getData = require('./getData');
 app.use(express.static('public'));
 
 app.get('/:kitap_ismi', async (req, res) => {
-   
+
     let json = await getData(req.params.kitap_ismi);
-    return res.json(json);    
+    return res.json(json);
 })
 
 
--- public/app.js
+++ public/app.js
@@ -8,7 +8,7 @@ async function getData(){
     
     let results = document.querySelector('.results');
     let row = document.createElement('div');
-    row.className = "row";
+    row.className = "row justify-items-center d-flex";
     let btn = document.getElementById('submit');
     btn.addEventListener('click', () =>{
 
@@ -22,55 +22,65 @@ async function getData(){
             }
         }
 
-        response.forEach(element => {
+        if(response.length > 0){
+            response.forEach(element => {
 
           
 
-            let brand = element.brand;
-            let name = element.name;
-            let price = element.price;
-            let image = element.picture;
-
-            let brandEl = document.createElement('div');
-            brandEl.className = 'brand';
-            let brandtxt = document.createElement('h5');
-            brandtxt.innerHTML = brand;
-            brandEl.appendChild(brandtxt); 
-
-            let nameEl = document.createElement('div');
-            nameEl.className = 'name';
-            let nametxt = document.createElement('a');
-            nametxt.setAttribute('href', element.url);
-            nametxt.innerHTML = name;
-            nameEl.appendChild(nametxt); 
-            
-            let priceEl = document.createElement('div');
-            priceEl.className = 'price';
-            let pricetxt = document.createElement('h5');
-            pricetxt.innerHTML = price + " TL.";
-            priceEl.appendChild(pricetxt); 
-
-            let imageEl = document.createElement('div');
-            imageEl.className = 'picture';
-            let priceimg = document.createElement('img');
-            priceimg.setAttribute('src', image);
-            imageEl.appendChild(priceimg);        
-
-            
-
-            let item = document.createElement('div');
-            item.className = 'item';
-
-            item.appendChild(imageEl);
-            item.appendChild(nameEl);
-            item.appendChild(brandEl);
-            item.appendChild(priceEl);
-
-            row.appendChild(item);
-            results.appendChild(row);
-
-
-    });
+                let brand = element.brand;
+                let name = element.name;
+                let price = element.price;
+                let image = element.picture;
+    
+                let brandEl = document.createElement('div');
+                brandEl.className = 'brand';
+                let brandtxt = document.createElement('h5');
+                brandtxt.innerHTML = brand;
+                brandEl.appendChild(brandtxt); 
+    
+                let nameEl = document.createElement('div');
+                nameEl.className = 'name';
+                let nametxt = document.createElement('a');
+                nametxt.setAttribute('href', element.url);
+                nametxt.innerHTML = name;
+                nameEl.appendChild(nametxt); 
+                
+                let priceEl = document.createElement('div');
+                priceEl.className = 'price';
+                let pricetxt = document.createElement('h5');
+                pricetxt.innerHTML = price + " TL.";
+                priceEl.appendChild(pricetxt); 
+    
+                let imageEl = document.createElement('div');
+                imageEl.className = 'picture';
+                let priceimg = document.createElement('img');
+                priceimg.setAttribute('src', image);
+                imageEl.appendChild(priceimg);        
+    
+                
+    
+                let item = document.createElement('div');
+                item.className = 'item';
+    
+                item.appendChild(imageEl);
+                item.appendChild(nameEl);
+                item.appendChild(brandEl);
+                item.appendChild(priceEl);
+    
+                row.appendChild(item);
+                results.appendChild(row);
+    
+    
+            });
+        }
+        else{
+            let info = document.createElement('h3');
+            info.className = "mt-3"
+            info.value = "Böyle bir kitap bulunmamaktadır.";
+            let c = document.createElement('div');
+            c.appendChild(info);
+            results.appendChild(c);
+        }
 
     
     }).catch(e => {
